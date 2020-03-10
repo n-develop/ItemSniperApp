e741f7ab20be67e95d5a0630138b4a572ebf1780
Lars Richter <github@n-develop.com>
2020-03-10 08:43:31 +0100

    Added project for E2E tests

---
+++ ItemSniperApp.EndToEndTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ItemSniperApp.EndToEndTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
